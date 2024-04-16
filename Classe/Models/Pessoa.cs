a19545e03aec45bd698f4a6ac4330f14a49b1c70
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-15 21:24:55 -0300

    criando classes e instanciando

---
+++ Classe/Models/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos"); ;
+        }
+    }
+}
---
+++ Classe/Program.cs
@@ -0,0 +1,13 @@
+﻿using Classe.Models;
+
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Wesley";
+p1.Idade = 27;
+p1.Apresentar();
+
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Beatriz";
+p2.Idade = 26;
+p2.Apresentar();
