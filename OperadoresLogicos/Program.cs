3da453b9e16179599a3ed35655f5175a6360e544
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-17 08:42:46 -0300

    operadores logicos OR, AND e NOT

---
+++ OperadoresLogicos/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+//operador(Pipe) OR (||)
+bool isMaiorIdade = true;
+bool possuiAltorizacaoDosPais = false;
+
+if (isMaiorIdade || possuiAltorizacaoDosPais)
+{
+    Console.WriteLine("Pode ir viajar");
+}
+else
+{
+    Console.WriteLine("Não tem autorização dos pais e não é maior de idade!");
+    Console.WriteLine("Não pode viajar");
+}
+
+//operador AND (&&)
+int presencaMinima = 75;
+double media = 8.5;
+if (presencaMinima >= 60 && media >= 7.0)
+{
+    Console.WriteLine($"Aprovado com {presencaMinima}% de presença e a media {media}");
+}
+else
+{
+    Console.WriteLine($"Reprovado com {presencaMinima}% de presença e a media {media}");
+}
+
+//operador NOT (!)
+bool possuiIngresso = false;
+if (!possuiIngresso)
+{
+    Console.WriteLine("Não é possivel entrar no estádio");
+}
+else
+{
+    Console.WriteLine("Pode entrar, possui ingresso");
+}
