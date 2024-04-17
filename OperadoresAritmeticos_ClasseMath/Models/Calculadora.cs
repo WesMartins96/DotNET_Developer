9fc1ab961397df395b9f856a5dfd858fce7e3ae1
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-17 09:34:16 -0300

    operadores aritmeticos e classe Math

---
+++ OperadoresAritmeticos_ClasseMath/Models/Calculadora.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresAritmeticos_ClasseMath.Models
+{
+    public class Calculadora
+    {
+
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        //usando a classe Math
+        //Potência
+        public void Potencia(int x, int y)
+        {
+            double potencia = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {potencia}");
+        }
+
+        //Funções trigonométricas
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {Math.Round(seno, 4)}");
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo}° = {Math.Round(coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo}° = {Math.Round(tangente, 4)}");
+        }
+
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"A raiz quadrada de {x} = {raiz}");
+        }
+
+    }
+}
---
+++ OperadoresAritmeticos_ClasseMath/Program.cs
@@ -0,0 +1,34 @@
+﻿using OperadoresAritmeticos_ClasseMath.Models;
+
+
+Calculadora calculadora = new Calculadora();
+
+calculadora.Somar(10, 10);
+calculadora.Subtrair(25, 10);
+calculadora.Multiplicar(5, 8);
+calculadora.Dividir(50, 5);
+
+//Incremento
+int numIncremento = 2;
+numIncremento++;
+Console.WriteLine(numIncremento);
+
+//Decremento
+int numDecremento = 5;
+numDecremento--;
+Console.WriteLine(numDecremento);
+
+
+//Classe Math
+//Potencia
+calculadora.Potencia(3, 3);
+
+//seno, coseno e tangente
+calculadora.Seno(30);
+calculadora.Coseno(30);
+calculadora.Tangente(30);
+
+//Raiz quadrada
+calculadora.RaizQuadrada(9);
+
+
