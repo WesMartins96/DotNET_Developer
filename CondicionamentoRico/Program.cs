8e7fd8803e46372cb0862f8c6c9e5975e72a230c
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-19 16:02:50 -0300

    Desafios de codigo numero um, realizados com sucesso

---
+++ CondicionamentoRico/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+/*Descrição
+Uma nova feature para um sistema bancário foi analisada pela equipe de desenvolvimento e será uma das tarefas a serem trabalhadas durante a sprint, 
+como desenvolvedor da empresa você recebeu os requisitos para a nova implementação que consiste em uma solução algorítmica que permita aos clientes realizarem 
+saques em caixas eletrônicos. No entanto, existem algumas regras a serem seguidas para garantir que um saque possa ser realizado com sucesso.
+
+Regras do saque:
+
+- Cada cliente digitará o valor do seu saldoTotal de sua conta bancária e o valorSaque.
+- Um saque só pode ser realizado se o saldoDisponível na conta for igual ou superior ao valor solicitado.
+- Se o saldo for suficiente, o valor solicitado deve ser subtraído do saldo disponível, indicando que o saque foi realizado.
+- Se o saldo for insuficiente, o saque não deve ser realizado e uma mensagem adequada deve ser exibida.
+
+Entrada
+A entrada consiste em dois valores inteiros que representam o total do saldo da conta e o valor do saque.
+
+Saída
+Se o saque for realizado com sucesso, exibir a mensagem "Saque realizado com sucesso! Novo saldo: {saldo}", onde {saldo} é o novo saldo disponível na conta.
+
+Se o saque não for possível devido a saldo insuficiente, exibir a mensagem "Saldo insuficiente. Saque nao realizado!"*/
+
+Console.Write("Digite o valor do saldo total: ");
+int saldoTotal = int.Parse(Console.ReadLine());
+Console.Write("Digite o valor do saque desejado: ");
+int valorSaque  = int.Parse(Console.ReadLine());
+Banco banco = new Banco(valorSaque, saldoTotal);
+
+
+
+//TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
+banco.Saque(valorSaque, saldoTotal);
+
+public class Banco
+{
+    public int Saldo { get; set; }
+    public int Valor { get; set; }
+
+    public Banco(int saldo, int valor)
+    {
+        Saldo = saldo;
+        Valor = valor;
+    }
+
+    public void Saque(int valor, int saldo)
+    {
+        saldo = saldo - valor;
+        if (saldo >= valor - saldo)
+        {
+            Console.WriteLine($"Saque realizado com sucesso. Novo saldo: {saldo}");
+        }else
+        {
+            Console.WriteLine("Saldo insuficiente. Saque nao realizado!");
+        }
+    }
+}
+
+
---
+++ ListaDeItens/Program.cs
@@ -0,0 +1,36 @@
+﻿
+/*Desafio
+Em um jogo de RPG, os personagens geralmente possuem uma lista de itens que podem ser utilizados durante o jogo. 
+Esses itens podem ser armas, armaduras, poções de cura, entre outros. É importante que o jogador tenha um controle desses itens 
+para poder utilizá-los no momento adequado.
+
+Crie um programa que permita cadastrar uma lista de itens que o personagem possui. A lista deve conter o valor fixo de 3 itens e deve ser exibida na tela.
+
+Entrada
+O programa deve solicitar ao usuário o nome dos 3 itens que o personagem possui. Cada item deve ser cadastrado separadamente.
+
+Saída
+Após receber os itens cadastrados pelo usuário, o programa deve exibir na tela a lista de itens que o personagem possui. A saída deve ter o seguinte formato:
+
+Lista de itens:
+- [item1]
+- [item2]
+- [item3]*/
+
+// Lista para armazenar os itens
+List<string> itens = new List<string>();
+
+// TODO: Solicite os itens ao usuário
+itens.Add(Console.ReadLine()); 
+itens.Add(Console.ReadLine()); 
+itens.Add(Console.ReadLine()); 
+
+// Exibe a lista de itens
+Console.WriteLine("Lista de itens:");
+foreach (string item in itens)
+{
+    Console.WriteLine($"- {item}");
+}
+
+
+
