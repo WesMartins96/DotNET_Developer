1610019b6e76f24392ded5e36d24a7955275fc24
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-19 07:56:19 -0300

    estruturas de repeticao e menu interativo

---
+++ EstruturasDeRepeticao/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+// FOR
+int numero = 10;
+for (int i = 1; i <= numero; i++)
+{
+    Console.WriteLine($"usado for: {numero} X {i} = {numero * i}");
+}
+
+
+// WHILE
+int contador = 1;
+while (contador <= 10)
+{
+    Console.WriteLine($"usando while: {numero} X {contador} = {numero * contador}");
+    contador++;
+    //podemos interromper o laço de repetição usando break;
+    if (contador == 6)
+    {
+        break;
+    }
+}
+
+
+// DO WHILE
+int soma = 0, num = 0;
+do
+{
+    Console.Write($"Usando Do While: Digite um número! vamos somar! (0 para parar): ");
+    num = int.Parse(Console.ReadLine());
+
+    soma += num;
+} while (num != 0);
+Console.WriteLine($"usando Do While: Total da soma dos números digitados é: {soma}");
+
+
+
+
+
---
+++ MenuInterativo/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+string opcao;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.WriteLine("[===[Menu]===]");
+    Console.WriteLine("1 - Cadastrar Cliente");
+    Console.WriteLine("2 - Buscar Cliente");
+    Console.WriteLine("3 - Apagar Cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    Console.Write($"Digite a sua opção: ");
+    opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1": Console.WriteLine("Cadastro de Cliente"); 
+            break;
+        case "2": Console.WriteLine("Busca de Cliente"); 
+            break;
+        case "3": Console.WriteLine("Apagar Cliente"); 
+            break;
+        case "4": Console.WriteLine("Programa encerrado");
+            exibirMenu = false;
+            //Environment.Exit(0); deste modo o sistema console se encerra por completo.
+            break;
+        default:
+            Console.WriteLine("Digite uma opção correta");
+            break;
+    }
+}
+
+Console.WriteLine("Programa executado por completo!");
