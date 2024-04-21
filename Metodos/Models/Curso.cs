fadb2efd2b945fd7d48fd917f4c3a2297cf609e2
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-21 16:21:02 -0300

    metodos, listas, classes e retornos

---
+++ Metodos/Models/Curso.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos.Models
+{
+    public class Curso
+    {
+        public string NomeCurso { get; set; }
+        public List<Aluno> Alunos { get; set; }
+
+
+        public void AdicionarAluno(Aluno aluno)
+        {
+            Alunos.Add(aluno);
+        }
+
+        public int QuantidadeAlunosMatriculados()
+        {
+            return Alunos.Count;
+        }
+
+        public void RemoverAluno(Aluno aluno)
+        {
+            Alunos.Remove(aluno);
+        }
+
+        public void ListarAlunos()
+        {
+            foreach (Aluno aluno in Alunos)
+            {
+                Console.WriteLine($"-> Nome: {aluno.Nome} | Idade: {aluno.Idade} | Curso: {NomeCurso}");
+            }
+        }
+
+    }
+}
---
+++ Metodos/Program.cs
@@ -0,0 +1,30 @@
+﻿using Metodos.Models;
+
+
+Aluno aluno1 = new Aluno();
+aluno1.Nome = "Wesley Martins";
+aluno1.Idade = 27;
+
+Aluno aluno2 = new Aluno();
+aluno2.Nome = "Beatriz Cristina";
+aluno2.Idade = 26;
+
+Aluno aluno3 = new Aluno();
+aluno3.Nome = "Gael Cachorro";
+aluno3.Idade = 4;
+
+Curso curso = new Curso();
+curso.NomeCurso = ".NET Developer AVANADE";
+curso.Alunos = new List<Aluno>();
+
+curso.AdicionarAluno(aluno1);
+curso.AdicionarAluno(aluno2);
+
+Curso cursoPet = new Curso();
+cursoPet.NomeCurso = "Curso PET";
+cursoPet.Alunos = new List<Aluno>();
+
+cursoPet.AdicionarAluno(aluno3);
+
+curso.ListarAlunos();
+cursoPet.ListarAlunos();
