fb95e017223b96512f5bde11e32f3053815e28c4
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-22 16:08:14 -0300

    datetime, formatando datas e horas e validacoes com tryparse

---
+++ Data_Hora/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+//Formatar o tipo DateTime
+using System.Globalization;
+
+DateTime data = DateTime.Now;
+
+Console.WriteLine(data);
+Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+Console.WriteLine(data.ToShortDateString());
+Console.WriteLine(data.ToString("dd/MM/yyyy"));
+Console.WriteLine(data.ToShortTimeString());
+
+//Podemos realizar a conversão de string para DateTime
+DateTime dataNasc = DateTime.Parse("16/06/1996 11:52:48");
+Console.WriteLine(dataNasc.ToString("dd/MM/yyyy HH:mm"));
+
+
+//validação se caso a data for invalida sem para a aplicação
+string dataValid = "32/06/2024 18:25:49";
+bool dataConvertida = DateTime.TryParseExact(dataValid, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date);
+if (dataConvertida)
+{
+    Console.WriteLine($"Convertido com sucesso {data}");
+}
+else
+{
+    Console.WriteLine($"Não é possivel converter! data incorreta!");
+}
