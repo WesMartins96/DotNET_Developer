51bf18e2576daec849cd1081a08812836dbe02eb
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-21 22:13:09 -0300

    construtores e parametros

---
+++ Construtores/Models/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa()
+        {
+            
+        }
+
+        public Pessoa(string nome, string sobrenome, int idade)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Idade = idade;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} {Sobrenome}, Idade: {Idade}";
+        }
+    }
+}
---
+++ Construtores/Program.cs
@@ -0,0 +1,8 @@
+﻿using Construtores.Models;
+
+
+Pessoa pessoa1 = new Pessoa(nome: "Wesley", sobrenome: "Martins", idade: 27);
+Pessoa pessoa2 = new Pessoa("Beatriz", "Cristina", 22);
+
+Console.WriteLine(pessoa1);
+Console.WriteLine(pessoa2);
