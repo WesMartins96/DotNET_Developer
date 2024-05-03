7c2adca0469a7142ed1fd9c27f59481193ff3009
Wesley Martins <wesley.mlopes@castgroup.com>
2024-05-03 06:46:10 -0300

    Excecoes e leitura de arquivos

---
+++ Excecoes/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+//Ler arquivo e usando exceções para captura de erros.
+
+try
+{
+    string path = @"C:\Users\wesley_lopes\Desktop\Codigos_Cursos\DotNET_Developer\Excecoes\Arquivos\arquivo_Leitura.txt";  //arquivoLeitura.txt
+    string[] linhas = File.ReadAllLines(path);
+
+    foreach (var linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine(ex.Message + " Arquivo não encontrado! ");
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine(ex.Message + " caminho da pasta não encontrado! ");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Chegou até aqui!");
+}
+
+
