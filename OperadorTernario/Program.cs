bc69505f13ed7ff94ef296a83eb21d280afc488d
Wesley Martins <wesley.mlopes@castgroup.com>
2024-05-04 04:07:05 -0300

    operador ternario

---
+++ OperadorTernario/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+//utilização do IF Ternário
+int numero = 2;
+
+
+//usando IF Tradicional
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"Numero {numero} é par");
+}
+else
+{
+    Console.WriteLine($"Numero {numero} é impar");
+}
+
+Console.WriteLine();
+
+
+//usando IF Ternário
+bool ehPar = numero % 2 == 0;
+Console.WriteLine($"Número {numero} é: {(ehPar ? "par" : "impar")} "); //IF Ternário  (condição ? true : false)
