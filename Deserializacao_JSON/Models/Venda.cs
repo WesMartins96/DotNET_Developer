2d6363c6ffcad9ba052624ff61b2d5143b04c514
Wesley Martins <wesley.mlopes@castgroup.com>
2024-05-07 03:21:44 -0300

    deserializacao de arquivo json para objetos, usando JsonProperty

---
+++ Deserializacao_JSON/Models/Venda.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Deserializacao_JSON.Models
+{
+    public class Venda
+    {
+        public int Id { get; set; }
+
+        //Caso o nome que venha do arquivo .json não respeite a sintaxe do C#, usamos atributo do JsonProperty do Newtonsoft.Json.
+        [JsonProperty("Nome_Produto")]
+        public string Produto { get; set; }
+
+        public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }
+    }
+}
---
+++ Deserializacao_JSON/Program.cs
@@ -0,0 +1,15 @@
+﻿using Deserializacao_JSON.Models;
+using Newtonsoft.Json;
+
+
+//Deserializar um objeto
+//Trabalhar as informações de um arquivo JSON para a linguagem C#
+
+string conteudoArquivoJson = File.ReadAllText(@"C:\Users\wesley_lopes\Desktop\Codigos_Cursos\DotNET_Developer\Deserializacao_JSON\Arquivos\vendas.json");
+
+List<Venda> vendas = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivoJson);
+
+foreach (var venda in vendas)
+{
+    Console.WriteLine($"Id: {venda.Id} / Produto: {venda.Produto} / Preço: {venda.Preco.ToString("F2")} / Data da Venda: {venda.DataVenda.ToString("dd/MM/yyyy")}");
+}
