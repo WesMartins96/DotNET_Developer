beb2b18d40ae02b23cf9e820b0f1afd0d0f7c53e
Wesley Martins <wesley.mlopes@castgroup.com>
2024-05-07 02:53:13 -0300

    serializacao JSON

---
+++ Serializacao_JSON/Models/Venda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacao_JSON.Models
+{
+    public class Venda
+    {
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }
+
+        public Venda(int id, string produto, decimal preco, DateTime dataVenda)
+        {
+            Id = id;
+            Produto = produto;
+            Preco = preco;
+            DataVenda = dataVenda;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id} / PRODUTO: {Produto} / PREÇO: {Preco.ToString("F2")} / DATA DA VENDA: {DataVenda.ToString("dd/MM/yyyy")}";
+        }
+    }
+}
---
+++ Serializacao_JSON/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serializacao_JSON.Models;
+using Newtonsoft.Json;
+
+//usando datas em formato JSON
+//Ao serializar o tipo DateTime o formato no .json se torna um (ISO 8601) -> Padroniza a representação de datas entre sistemas.
+DateTime dataAtual = DateTime.Now;
+
+//serializando uma coleção(lista)
+Venda venda1 = new Venda(1, "Peças de PC", 2500.00M, dataAtual); 
+Venda venda2 = new Venda(2, "Licença de Software", 399.98M, dataAtual);
+Venda venda3 = new Venda(3, "Water Cooler", 79.99M, dataAtual);
+
+List<Venda> listaVendas = new List<Venda>();
+listaVendas.Add(venda1);
+listaVendas.Add(venda2);
+listaVendas.Add(venda3);
+
+
+//Transformar dado em formato JSON
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+Console.WriteLine(serializado);
+
+
+//Escrever/criar um arquivo .json
+File.WriteAllText(@"C:\Users\wesley_lopes\Desktop\Codigos_Cursos\DotNET_Developer\Serializacao_JSON\Arquivos\arquivo.json", serializado);
