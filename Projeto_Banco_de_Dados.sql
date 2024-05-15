cfd75a0eb5c594c5602427839534e84277027b03
Wesley Martins <wesley.mlopes@castgroup.com>
2024-05-14 23:53:34 -0300

    Projeto banco de dados completo

---
+++ Projeto_Banco_de_Dados.sql
@@ -0,0 +1,68 @@
+﻿--Desafio de projeto
+--Para este desafio, você precisará usar seus conhecimentos adquiridos no módulo de banco de dados, da trilha .NET da DIO.
+
+--Contexto
+--Você é responsável pelo banco de dados de um site de filmes, onde são armazenados dados sobre os filmes e seus atores. 
+--Sendo assim, foi solicitado para que você realize uma consulta no banco de dados com o objetivo de trazer alguns dados para análises.
+
+--Proposta
+--Você precisará realizar 12 consultas ao banco de dados, cada uma retornando um tipo de informação. O seu banco de dados está modelado da seguinte maneira:
+
+--1 - Buscar o nome e ano dos filmes
+SELECT Nome, Ano FROM Filmes;
+
+--2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT Nome, Ano FROM Filmes ORDER BY Ano ASC;
+
+--3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De volta para o futuro';
+
+--4 - Buscar os filmes lançados em 1997
+SELECT * FROM Filmes WHERE Ano = 1997;
+
+--5 - Buscar os filmes lançados APÓS o ano 2000
+SELECT * FROM Filmes WHERE Ano > 2000;
+
+--6 - Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+SELECT * FROM Filmes WHERE Duracao > 100 AND Duracao < 150 ORDER BY Duracao ASC;
+
+--7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
+SELECT Ano, COUNT(*) Quantidade 
+FROM Filmes 
+GROUP BY Ano 
+ORDER BY Quantidade DESC;
+
+--8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+SELECT PrimeiroNome, UltimoNome, Genero FROM Atores WHERE Genero = 'M';
+
+--9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+SELECT PrimeiroNome, UltimoNome, Genero FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome;
+
+--10 - Buscar o nome do filme e o gênero
+SELECT 
+	F.Nome,
+	G.Genero
+FROM
+	Filmes F
+INNER JOIN Generos G on F.Id = G.Id;
+
+--11 - Buscar o nome do filme e o gênero do tipo "Mistério"
+SELECT 
+	F.Nome,
+	G.Genero
+FROM
+	Filmes F
+INNER JOIN Generos G on F.Id = G.Id
+WHERE G.Genero = 'Mistério';
+
+--12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+SELECT 
+	F.Nome,
+	A.PrimeiroNome,
+	A.UltimoNome,
+	E.Papel
+FROM
+	Filmes F
+INNER JOIN Atores A on F.Id = A.Id
+INNER JOIN ElencoFilme E on A.Id = E.Id;
+
