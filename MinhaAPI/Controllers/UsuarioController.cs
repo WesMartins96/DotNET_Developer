5c84ed8c3e791466cf3f5fe8304c74ef99c1a78e
Wesley Martins <wesley.mlopes@castgroup.com>
2024-05-17 01:36:12 -0300

    construcao de uma API com a interface do SWAGGER.

---
+++ MinhaAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString(),
+            };
+
+            return Ok(obj);
+        }
+
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome}, seja bem vindo!";
+
+            return Ok(new { mensagem });
+
+        }
+
+
+    }
+}
